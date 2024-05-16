f1d96c9adb095cc8dc2b42daf4d99aebc0e110a6
Prakhar Tripathi <110407222+gurawliprakhar@users.noreply.github.com>
2024-05-16 15:38:08 +0530

    updated few lines checking issues so i can add them to frontend

--- backend/src/main/java/com/velamdir/backend/pdfassistent/PdfAssistentConfig.java
+++ backend/src/main/java/com/velamdir/backend/pdfassistent/PdfAssistentConfig.java
@@ -24,8 +24,8 @@ public class PdfAssistentConfig {
 
     @Bean
     public AstraDbEmbeddingStore astraDbEmbeddingStore() {
-        String astraToken = "AstraCS:HTzFoKOETjAnBMkLzAmvFbzT:441074cbf5d93b0f77c86e9f16f1e56144ee9faa0fe1550818e698b3e0c6b43d";
-        String databaseId = "74b9e3b0-3aaa-4577-9a02-8d271e6a9912";
+        String astraToken = "";
+        String databaseId = "";
 
         return new AstraDbEmbeddingStore(AstraDbEmbeddingConfiguration
                 .builder()
